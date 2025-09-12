67cdf4f72d492404f28e5342a4f56937169b93a4
elsakti <svkakazone@gmail.com>
2025-09-12 07:08:31 +0700

    feat: basic front-end

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -5,7 +5,7 @@ const NavbarComponent = () => {
   return (
     <Navbar variant="dark" expand="lg">
       <Container>
-      <Navbar.Brand href="#home"><strong>Kasir</strong> App</Navbar.Brand>
+      <Navbar.Brand href="#home"><strong>3124510030</strong> Rakha Bagus Saktiawan</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
